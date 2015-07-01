72391661a8511cb0fc84b6bfb9360368aa5849c3
emir-munoz <emir@emunoz.org>
2015-07-01 23:07:28 +0100

    Cleaning dashboard branch

--- main/rdf-to-fuseki/src/main/java/org/xenei/galway2020/sink/fuseki/auth/AuthFactory.java
+++
@@ -1,9 +0,0 @@
-package org.xenei.galway2020.sink.fuseki.auth;
-
-import org.apache.commons.configuration.Configuration;
-import org.apache.jena.atlas.web.auth.HttpAuthenticator;
-
-public interface AuthFactory {
-	
-	HttpAuthenticator create( Configuration cfg );
-}
